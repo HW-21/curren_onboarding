1a59c7041b4513bbf60b0040bb2ef29ef7892680
HW-21 <96047687+HW-21@users.noreply.github.com>
2022-05-25 09:52:12 +0800

    Create address.ts

---
+++ fixtures/data/address.ts
@@ -0,0 +1,21 @@
+export default {
+  address1: {
+    addressLine_1: 'AL',
+    addressLine_2: 'AL',
+    city: 'CT',
+    stateRegionDistrict: 'SRD',
+    postalCode: 'PC',
+    country: 'Hong Kong',
+  },
+  address2: {
+    addressLine_1: 'AL',
+    addressLine_2: 'AL',
+    city: 'CT',
+    stateRegionDistrict: 'SRD',
+    postalCode: 'PC',
+    country: 'China',
+  },
+  isSameAsRegisteredBusinessAddress: {
+    isSameAsRegisteredBusinessAddress: true,
+  },
+}
